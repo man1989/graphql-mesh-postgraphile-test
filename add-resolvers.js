35a7476574cbd6aba333d785c4175f7d36819784
Manish Kumar <manishkumar@manish.local>
2020-10-05 05:04:16 +0530

    unable to all api function if there are multiple sources

--- add-resolvers.js
+++ add-resolvers.js
@@ -1,12 +1,8 @@
 const resolvers = {
     Query: {
         cities: async (root, args, context, info) => {
-            console.log(args, context.graphile.api.allCities);
-            /**
-             * below call returns Iterator
-             */
             const data = await context.graphile.api.allCities(args);
-            console.log(data);
+            console.log(data.nodes);
             return data;
         }
     }
